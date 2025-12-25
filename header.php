260265aaca7f509a6e4132f086b0065447e82e90
ali-reh <youssef_ahz@hotmail.com>
2025-12-25 19:03:50 +0200

    Fixed

---
+++ header.php
@@ -0,0 +1,42 @@
+<?php
+// Shared header include: navbar + tooltip init
+?>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light shadow px-lg-5" id="navbar">
+        <div class="container d-flex flex-row align-items-center justify-content-between">
+            <a href="index.php" class="brand navbar-brand order-2 order-lg-1">
+                BRAND
+            </a>
+
+            <a class="navbar-toggler border-0 order-1" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="bi bi-list menu-button"></i>
+            </a>
+
+            <div class="collapse navbar-collapse order-4 order-lg-2" id="collapsibleNavbar">
+                <ul class="navbar-nav mx-auto" id="pages-links">
+                    <li class="nav-item"><a href="index.php" class="nav-link active">Home</a></li>
+                    <li class="nav-item"><a href="shop.php" class="nav-link">Shop</a></li>
+                </ul>
+            </div>
+
+            <ul class="navbar-nav d-flex flex-row gap-3 order-3 justify-content-center align-items-center" id="nav-icons">
+                <li class="nav-item">
+                    <a href="#" class="nav-link active" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Cart" aria-label="Cart">
+                        <i class="bi bi-cart2"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link active" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Sign in" aria-label="Sign in">
+                        <i class="bi bi-person"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const tooltipTriggerList = Array.prototype.slice.call(document.querySelectorAll('#nav-icons [data-bs-toggle="tooltip"]'));
+        tooltipTriggerList.forEach(el => new bootstrap.Tooltip(el));
+    });
+    </script>
+</header>
--- index.php
+++ index.php
@@ -23,45 +23,7 @@
     <link rel="stylesheet" href="style/featured.css">
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light shadow px-lg-5" id="navbar">
-            <div class="container d-flex flex-row align-items-center justify-content-between">
-                <a href="index.php" class="brand navbar-brand order-2 order-lg-1">
-                    BRAND
-                </a>
-
-                <a class="navbar-toggler border-0 order-1" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="bi bi-list menu-button"></i>
-                </a>
-
-                <div class="collapse navbar-collapse order-4 order-lg-2" id="collapsibleNavbar">
-                    <ul class="navbar-nav mx-auto" id="pages-links">
-                        <li class="nav-item"><a href="index.php" class="nav-link active">Home</a></li>
-                        <li class="nav-item"><a href="shop.php" class="nav-link">Shop</a></li>
-                    </ul>
-                </div>
-
-                <ul class="navbar-nav d-flex flex-row gap-3 order-3 justify-content-center align-items-center" id="nav-icons">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link active" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Cart" aria-label="Cart">
-                            <i class="bi bi-cart2"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link active" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Sign in" aria-label="Sign in">
-                            <i class="bi bi-person"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            const tooltipTriggerList = Array.prototype.slice.call(document.querySelectorAll('#nav-icons [data-bs-toggle="tooltip"]'));
-            tooltipTriggerList.forEach(el => new bootstrap.Tooltip(el));
-        });
-        </script>
-    </header>
+    <?php include __DIR__ . '/header.php'; ?>
     <section class="hero-section py-5 py-md-6 d-flex flex-column justify-content-center">
         <div class="container">
             <div class="hero-content mx-auto d-flex flex-column align-items-start">
@@ -72,7 +34,7 @@
                 </h1>
 
                 <p class="hero-text mt-4 primary-font">
-                    Curated essentials that bring beauty and function to your daily rituals.
+                    Curated essentials that bring beauty and function to your daily <br>
                     Quality craftsmanship, timeless style.
                 </p>
 
@@ -89,7 +51,7 @@
         </div>
     </section>
     <section id="featured-items">
-
+        
     </section>
 </body>
 </html>
